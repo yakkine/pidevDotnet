9aa374d6a7e69675e0d0919da9d9a07472d8ae3f
yakkine <yakine.kochbati@esprit.tn>
2017-12-06 15:52:31 +0100

    projet dotnet avec des entites reverse

---
+++ Pidev/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Pidev.Startup))]
+namespace Pidev
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
